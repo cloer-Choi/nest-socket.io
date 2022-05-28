2d1bbf12c2813cdebd6f233cace39df22edc5285
cloer <chltjrrb97@gmail.com>
2022-05-27 21:49:49 -0400

    ADD: pug

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { All, Controller, Get, Redirect, Render } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  getIndex() {
+    return;
+  }
+
+  @All('*')
+  @Redirect('/')
+  notFoundExeption() {
+    return;
   }
 }
